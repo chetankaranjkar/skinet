04dd38379da33ce48a04ce48015d95a30b3b78d7
Chetan Karanjkar <ckaranjkar@ad.medrio.com>
2022-07-29 01:35:15 +0530

    GUI Complete

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
+import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/Interceptors/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/Interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,9 +22,13 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule,
+    NgxSpinnerModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { delay, map } from 'rxjs/operators'
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -14,29 +15,32 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/'
   constructor(private http: HttpClient) { }
 
-  
+
   getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
-    if (shopParams.brandId!==0) {
+    if (shopParams.brandId !== 0) {
       params = params.append('brandid', shopParams.brandId.toString());
     }
-    if (shopParams.typeId!==0) {
+    if (shopParams.typeId !== 0) {
       params = params.append('typeid', shopParams.typeId.toString());
     }
-    if(shopParams.search){
-      params=params.append('search',shopParams.search);
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
     }
     params = params.append('sort', shopParams.sort);
-    params=params.append('pageIndex',shopParams.pageNumber.toString());
-    params=params.append('pageSize',shopParams.pageSize.toString());
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
 
-    
+
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
       .pipe(
         map(response => {
           return response.body;
         })
       );
+  }
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl+'products/'+id);
   }
 
   getBrands() {
@@ -47,5 +51,5 @@ export class ShopService {
     return this.http.get<IType[]>(this.baseUrl + 'products/types')
   }
 
-  
+
 }
