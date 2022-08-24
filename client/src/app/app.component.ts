d4fdec2bbe8f67e2e559d7bb97d43445d1a540c6
Chetan Karanjkar <ckaranjkar@ad.medrio.com>
2022-08-24 09:39:19 +0530

    User Login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 import { IPagination } from './shared/models/pagination';
 import { IProduct } from './shared/models/product';
@@ -13,18 +14,37 @@ export class AppComponent implements OnInit {
 
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService,private accountService:AccountService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    console.log('onIt');
+    this.loadBasket();
+    this.loadcurrentUser();
+  }
+
+  loadcurrentUser(){
+    console.log(localStorage.getItem('token'));
+    const token = localStorage.getItem('token');
+   
+      this.accountService.loadCurrentUser(token).subscribe(()=>{
+        console.log('loaded user');
+      },error=>{
+        console.log(error);
+       
+      });
+    
+  }
+
+  loadBasket(){
     const basketid = localStorage.getItem('basket_id');
     if (basketid) {
       this.basketService.getBasket(basketid).subscribe(()=>{
-        console.log('iniyialize basket')
+        console.log('initialize basket')
       },error=>{
         console.log(error);
        
       });
     }
   }
-
 }
